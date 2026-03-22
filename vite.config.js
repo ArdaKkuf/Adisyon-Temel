df3106e1e587d5e4ddf63848ed1a45b99da67184
Ardakuf <akuf1977@gmail.com>
2026-03-22 03:12:44 +0300

    Fix: Add base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Adisyon-Temel/',
   build: {
     outDir: 'docs',
   },
